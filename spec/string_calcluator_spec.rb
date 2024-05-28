493b97dcbb2bb8361ceb567d432258a8a91d472e
Bhaskar <bhaskar19191@gmail.com>
2024-05-28 13:24:45 +0530

    Add test case and implementation for single number

--- spec/string_calcluator_spec.rb
+++ spec/string_calcluator_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe StringCalculator do
     it "returns 0 for an empty string" do
         expect(StringCalculator.add("")).to eq(0)
     end
+
+    it "returns the number if we give  a single number" do
+        expect(StringCalculator.add("134")).to eq(134)
+    end
 end
